668f98476ed70e4d0b0cf6d2dfd756a0df5b57f3
hdgelman <hdg18@icloud.com>
2022-08-31 10:16:25 -0500

    Local Storage practice

--- storage.js
+++ storage.js
@@ -1,4 +1,12 @@
-localStorage = {
-    name: "Fluffy",
-    age: 65
-}
+// console -> Application -> Storage -> Local Storage -> file
+localStorage.setItem('BUNNY', 'Hoppy')
+const bunny = document.getElementById('bunny')
+const bunnyName = localStorage.getItem('bunny')
+bunny.innerHTML = bunnyName ? bunnyName : 'No bunny found';
+localStorage.removeItem('bunny')
+
+localStorage.setItem('user', {
+    name: 'Bobby',
+    age: 50
+})
+console.log(localStorage)
